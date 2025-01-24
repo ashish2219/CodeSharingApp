ecd38e21411ba25ed0c04c0582803cf98ccb5b20
Ashish Kunal <ashishkunal678@gmail.com>
2025-01-24 19:02:23 +0530

    added multipe language functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,59 @@ import Editor from "@monaco-editor/react";
 import "./globals.css";
 
 export default function page() {
+  const defaultHTML = `<html>
+  <head>
+    <title>HTML Sample</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style type="text/css">
+      h1 {
+        color: #CCA3A3;
+      }
+    </style>
+    <script type="text/javascript">
+      alert("I am a sample... visit devChallengs.io for more projects");
+    </script>
+  </head>
+  <body>
+    <h1>Heading No.1</h1>
+    <input disabled type="button" value="Click me" />
+  </body>
+</html>`;
+
+  const defaultJs = `console.log("Hello World!");`;
+
+  const defaultPython = `print("Hello World!");`;
+
+
   const [langDropdown, setLangDropdown] = useState(false); //language dropdown state
   const [themeDropdown, setThemeDropdown] = useState(false); //theme dropdown state
   const [editorTheme, setEditorTheme] = useState("vs-light"); //editor theme state
   const [containerTheme, setContainerTheme] = useState("light"); //container theme state
+  const [selectedLanguage, setSelectedLanguage] = useState("html"); //editor language state
+  const [editorValue, setEditorValue] = useState(defaultHTML);
 
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
   };
 
+  const handleLanguageChange = (newLanguage) => {
+    setSelectedLanguage(newLanguage);
+
+    if (newLanguage === "html") {
+      setEditorValue(defaultHTML); //update selected language
+    } else if (newLanguage === "javascript") { // update default content based on language
+      setEditorValue(defaultJs);
+    } else if (newLanguage === "python") {
+      setEditorValue(defaultPython);
+    }
+    setLangDropdown(false);
+  }
+
   const handleThemeChange = (theme) => { //handling theme state
     if (theme === "Dark") {
-      setEditorTheme("hc-black");
-      setContainerTheme("dark");
+      setEditorTheme("hc-black"); //update editor theme
+      setContainerTheme("dark"); //update container theme
     } else {
       setEditorTheme("vs-light");
       setContainerTheme("light");
@@ -29,7 +69,7 @@ export default function page() {
     <div>
 
     <div className="logo-container">
-      <img src="/images/NoteCodeLogo.svg" alt="" />
+      <img src="/images/NoteCodeLogo.svg" alt="logo" />
       <h2>Create {`&`} Share</h2>
       <h1>Your Code easily</h1>
     </div>
@@ -54,25 +94,9 @@ export default function page() {
             <Editor
               theme={editorTheme} //Dynamic theme
               height="65vh"
-              defaultLanguage="html"
-              defaultValue={`<html>
-  <head>
-    <title>HTML Sample</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <style type="text/css">
-      h1 {
-        color: #CCA3A3;
-      }
-    </style>
-    <script type="text/javascript">
-      alert("I am a sample... visit devChallengs.io for more projects");
-    </script>
-  </head>
-  <body>
-    <h1>Heading No.1</h1>
-    <input disabled type="button" value="Click me" />
-  </body>
-</html>`}
+              language={selectedLanguage}
+              value={editorValue}
+              onChange={(newValue) => setEditorValue(newValue)} // 
             />
 
           </div>
@@ -82,13 +106,17 @@ export default function page() {
 
               <div className="dropdown">
                 <button type="button" className="leftSideButton" onClick={() => {setLangDropdown(!langDropdown)}}>
-                  HTML
+                  {selectedLanguage === "html" ? "HTML"
+                   : selectedLanguage === "javascript" ? "JavaScript"
+                   : "Python"
+                  }
                   <img src="/images/down arrow.svg" alt="" />
                 </button>
                 {langDropdown && (
                   <div className="dropdownmenu">
-                    <div className="dropdownitem">JavaScript</div>
-                    <div className="dropdownitem">Python</div>
+                    <div className="dropdownitem" onClick={() => handleLanguageChange("html")}>HTML</div>
+                    <div className="dropdownitem" onClick={() => handleLanguageChange("javascript")}>JavaScript</div>
+                    <div className="dropdownitem" onClick={() => handleLanguageChange("python")}>Python</div>
                   </div>
                 )} 
               </div>
